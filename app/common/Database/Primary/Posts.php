602022c60c950b4cc79c4ed6dc9d353aa50f932c
Maria-cpp <vipisbd@gmail.com>
2022-01-05 18:25:31 +0500

    posts

---
+++ app/common/Database/Primary/Posts.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Common\Database\Primary;
+
+use App\Common\Database\AbstractAppTable;
+use App\Common\Exception\AppException;
+use App\Common\Kernel;
+use App\Common\Posts\Post;
+use Comely\Database\Exception\ORM_ModelNotFoundException;
+use Comely\Database\Schema\Table\Columns;
+use Comely\Database\Schema\Table\Constraints;
+
+/**
+ * Class Users
+ * @package App\Common\Database\Primary
+ */
+class Posts extends AbstractAppTable
+{
+    public const NAME = 'posts';
+    public const MODEL = 'App\Common\Posts\Post';
+    public const BINARY_OBJ_SIZE = 4096;
+
+    /**
+     * @param Columns $cols
+     * @param Constraints $constraints
+     */
+    public function structure(Columns $cols, Constraints $constraints): void
+    {
+        $cols->defaults("ascii", "ascii_general_ci");
+
+        $cols->int("id")->bytes(4)->unSigned()->autoIncrement();
+        $cols->varchar("title")->length(255)
+            ->charset("utf8mb4")->collation("uft8mb4_0900_ai_ci");
+        $cols->string("content")->length(512)
+            ->charset("utf8mb4")->collation("uft8mb4_0900_ai_ci");
+        
+        $cols->varchar("image_url")->length(255)
+            ->charset("utf8mb4")->collation("uft8mb4_0900_ai_ci");        
+     
+        $cols->timestamp("created_at")->bytes(4)->unSigned();
+        $cols->timestamp("updated_at")->bytes(4)->unSigned();
+        $cols->varchar("category")->length(255)
+        ->charset("utf8mb4")->collation("uft8mb4_0900_ai_ci");
+        $constraints->foreignKey("user_id")->table(Users::NAME, "id");
+    }
+   
+}
