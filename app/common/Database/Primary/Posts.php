2f77c5b791ac257299ea84ec8e1570803a39087d
Maria-cpp <vipisbd@gmail.com>
2022-01-06 12:16:41 +0500

    updated posts table file

--- app/common/Database/Primary/Posts.php
+++ app/common/Database/Primary/Posts.php
@@ -31,17 +31,17 @@ class Posts extends AbstractAppTable
 
         $cols->int("id")->bytes(4)->unSigned()->autoIncrement();
         $cols->string("title")->length(255)
-            ->charset("utf8mb4")->collation("uft8mb4_0900_ai_ci");
+            ->charset("utf8mb4")->collation("utf8mb4_general_ci");
         $cols->string("content")->length(512)
-            ->charset("utf8mb4")->collation("uft8mb4_0900_ai_ci");
+            ->charset("utf8mb4")->collation("utf8mb4_general_ci");
         
         $cols->string("image_url")->length(255)
-            ->charset("utf8mb4")->collation("uft8mb4_0900_ai_ci");        
+            ->charset("utf8mb4")->collation("utf8mb4_general_ci");        
      
         $cols->int("created_at")->bytes(4)->unSigned();
         $cols->int("updated_at")->bytes(4)->unSigned();
         $cols->string("category")->length(255)
-        ->charset("utf8mb4")->collation("uft8mb4_0900_ai_ci");
+        ->charset("utf8mb4")->collation("utf8mb4_general_ci");
         $constraints->foreignKey("user_id")->table(Users::NAME, "id");
     }
    
